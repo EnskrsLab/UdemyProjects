18f55c12e3a499d79e9882b7ff8e73d8ac1af71f
EnskrsLab <eneskaras@hotmail.com>
2019-07-23 13:32:59 +0300

    lesson login_activty_ui completed

--- app/src/main/java/com/example/smack/MainActivity.kt
+++ app/src/main/java/com/example/smack/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.smack
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
@@ -45,7 +46,8 @@ class MainActivity : AppCompatActivity() {
 
     fun loginBtnNavClicked(view: View)
     {
-
+        val loginIntent = Intent(this, LoginActivity::class.java)
+        startActivity(loginIntent)
     }
 
     fun addChannelClicked(view: View)
@@ -57,5 +59,5 @@ class MainActivity : AppCompatActivity() {
     {
 
     }
-    
+
 }
